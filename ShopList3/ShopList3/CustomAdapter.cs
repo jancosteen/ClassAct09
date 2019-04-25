7227b45410fa18b09cd08313fbaef7d0e30a46d1
Janco Steenkamp <jancosteenkamp3@gmail.com>
2019-04-25 13:17:58 +0200

    Finished Project

---
+++ ShopList3/ShopList3/CustomAdapter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ShopList3.Helper
+{
+    class CustomAdapter : BaseAdapter
+    {
+        private MainActivity mainActivity;
+        private List<string> itemList;
+        private DbHelper dbHelper;
+        public CustomAdapter(MainActivity mainActivity, List<string> itemList, DbHelper dbHelper)
+        {
+            this.mainActivity = mainActivity;
+            this.itemList = itemList;
+            this.dbHelper = dbHelper;
+        }
+        public override int Count { get { return itemList.Count; } }
+        public override Java.Lang.Object GetItem(int position)
+        {
+            return position;
+        }
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            LayoutInflater inflater = (LayoutInflater)mainActivity.GetSystemService(Context.LayoutInflaterService);
+            View view = inflater.Inflate(Resource.Layout.row, null);
+            TextView txtTask = view.FindViewById<TextView>(Resource.Id.item_title);
+            Button btnDelete = view.FindViewById<Button>(Resource.Id.btnDelete);
+            txtTask.Text = itemList[position];
+            btnDelete.Click += delegate
+            {
+                string task = itemList[position];
+                dbHelper.deleteTask(task);
+                mainActivity.LoadTaskList(); // Reload Data  
+            };
+            return view;
+        }
+    }
+}
---
+++ ShopList3/ShopList3/obj/Debug/81/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -0,0 +1,14 @@
+package mono.android.app;
+
+public class XamarinAndroidEnvironmentVariables
+{
+	// Variables are specified the in "name", "value" pairs
+	public static final String[] Variables = new String[] {
+		"MONO_LOG_LEVEL", "info",
+		"XAMARIN_BUILD_ID", "7a21f8db-c6eb-4a92-a3a5-a56f74971821",
+		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
+		"XA_TLS_PROVIDER", "btls",
+		"MONO_GC_PARAMS", "major=marksweep-conc",
+
+	};
+}
